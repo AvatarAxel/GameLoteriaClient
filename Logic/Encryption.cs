617cb2928462e7baf04eb3bcb3b09e9ea00422aa
AvatarAxel <zs20015691@estudiantes.uv.mx>
2022-10-25 01:05:08 -0500

    22.10.25

---
+++ Logic/Encryption.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Logic
+{
+    public class Encryption
+    {
+        public string HashPassword256(string password) 
+        {
+            using (SHA256 sha256 = SHA256.Create()) 
+            {
+                string hashedPassword;
+                Byte[] passwordHashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < passwordHashedBytes.Length; i++)
+                {
+                    stringBuilder.Append(passwordHashedBytes[i].ToString("x2"));
+                }
+                hashedPassword = stringBuilder.ToString();
+                return hashedPassword;
+            }
+        }
+    }
+}
--- View/View/Login.xaml.cs
+++ View/View/Login.xaml.cs
@@ -11,15 +11,13 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Logic;
 
 
 namespace View
 {
-    /// <summary>
-    /// Interaction logic for Login.xaml
-    /// </summary>
     public partial class Login : Window, ServiceReference.IAuthenticationServiceCallback
     {
         public Login()
@@ -30,28 +28,40 @@ namespace View
         {
             WindowState = WindowState.Minimized;
         }
-
         private void btnClose_Click(Object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
-
         public void ReponseAuthenticated(bool result)
         {
-            this.ResultadoTextBox.Text = result.ToString();
+            if (result)
+            {
+                MainWindow mainWindow = new MainWindow();
+                Close();
+                mainWindow.Show();                
+            }
+            else 
+            {                
+                MessageBox.Show("El username y/o password que ingreso no se encuentra(n) registrados, verifique que sean los datos correctos o regístrese", "Atención", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
-
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            InstanceContext context = new InstanceContext(this);
-            ServiceReference.AuthenticationServiceClient client = new ServiceReference.AuthenticationServiceClient(context);
-            string username;
-            username = txtUser.Text;
-            string password;
-            password = txtPassword.Password;
-            client.IsAuthenticated(username, password);
-            //MessageBox.Show("Mensaje", "Titulo",MessageBoxButton.OKCancel,MessageBoxImage.Warning);
-            
+            if (string.IsNullOrWhiteSpace(txtPassword.Password) || string.IsNullOrWhiteSpace(txtUser.Text))
+            {
+                MessageBox.Show("Campos vacios, intentelo de nuevo", "Atención", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else 
+            {
+                InstanceContext context = new InstanceContext(this);
+                ServiceReference.AuthenticationServiceClient client = new ServiceReference.AuthenticationServiceClient(context);
+                string username;
+                string hashedPassword;
+                username = txtUser.Text;
+                Encryption encryption = new Encryption();
+                hashedPassword = encryption.HashPassword256(txtPassword.Password);
+                client.IsAuthenticated(username, txtPassword.Password);
+            }    
         }
     }
 }
