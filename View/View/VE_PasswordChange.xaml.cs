6be3a11754aad2b72767d4b95a12ecaf4b89520a
Ale-VP <aavp1603@hotmail.com>
2022-11-03 12:19:51 -0600

    03.11.2022

--- View/View/VE_PasswordChange.xaml.cs
+++ View/View/VE_PasswordChange.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -35,10 +37,27 @@ namespace View
 
         private void BtnUpdateDat_Click(object sender, RoutedEventArgs e)
         {
-            VE_VerificationEmail changePassword = new VE_VerificationEmail();
-            changePassword.ShowDialog();
+            if (string.IsNullOrWhiteSpace(txtPassword.Password) || string.IsNullOrWhiteSpace(txtEmail.Text) || string.IsNullOrWhiteSpace(txtPasswordValidation.Password))
+            {
+                MessageBox.Show("Campos vacios, intentelo de nuevo", "Atención", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                string email;
+                string passwordValidation;
+                email = txtEmail.Text;
+                passwordValidation = txtPasswordValidation.Password;
+                string hashedPassword;
 
-            //Servidor
+                VE_VerificationEmail changePassword = new VE_VerificationEmail();
+                changePassword.ShowDialog();
+                
+
+
+                Encryption encryption = new Encryption();
+                hashedPassword = encryption.HashPassword256(txtPassword.Password);
+            }
+
         }
     }
 }
